5b8a56dc073e833e0b63ab8900092b483706a294
minjin63 <minjin_99@kookmin.ac.kr>
2020-11-09 04:05:21 +0900

    Until multiplication

---
+++ 최강창민ver3/test.h
@@ -0,0 +1,23 @@
+#pragma once
+//
+//  test.h
+//  Changmin's library
+//
+//  Created by √÷∞≠√¢πŒ on 2020/11/09.
+//  Copyright 2020 √÷∞≠√¢πŒ. All rights reserved.
+//
+
+#ifndef test_h
+#define test_h
+
+void COMPARE_test();
+void SHIFT_test();
+void REDUCTION_test();
+
+void ADD_test();
+void SUB_test();
+void MUL_test();
+void Karatsuba_test();
+void SQUC_test();
+
+#endif /* test_h */
