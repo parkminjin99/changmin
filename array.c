8cd364ee4d33adb2eddf1f1add37f002acab3f85
africanssong <73681875+africanssong@users.noreply.github.com>
2020-12-05 14:12:53 +0900

    Add files via upload

--- array.c
+++ array.c
@@ -8,17 +8,17 @@
 
 #include "array.h"
 
-void array_init(word* a, int len) // 배열을 0으로 초기화하는 함수
+void array_init(word* a, int len)                       // 배열을 0으로 초기화하는 함수
 {
     memset(a, 0, sizeof(word) * len);
 }
 
-void array_copy(word* dst, const word* src, int len) // dst배열에 src배열을 복사하는 함수
+void array_copy(word* dst, const word* src, int len)    // dst배열에 src배열을 복사하는 함수
 {
     memcpy(dst, src, len * sizeof(word));
 }
 
-void array_rand(word* dst, int wordlen) // 배열에 랜덤하게 값을 입력하는 함수
+void array_rand(word* dst, int wordlen)                 // 배열에 랜덤하게 값을 입력하는 함수
 {
     unsigned char* p = (unsigned char*)dst;
     int cnt = wordlen * sizeof(word);
--- array.h
+++ array.h
@@ -13,8 +13,8 @@
 #include <string.h>
 #include "changmin_library.h"
 
-void array_init(word* a, int len); // 배열을 0으로 초기화하는 함수
+void array_init(word* a, int len);                    // 배열을 0으로 초기화하는 함수
 void array_copy(word* dst, const word* src, int len); // dst배열에 src배열을 복사하는 함수
-void array_rand(word* dst, int wordlen); // 배열에 랜덤하게 값을 입력하는 함수
+void array_rand(word* dst, int wordlen);              // 배열에 랜덤하게 값을 입력하는 함수
 
 #endif /* array_h */
